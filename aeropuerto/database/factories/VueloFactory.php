62a90b5fa9ae1ad89f8207e5c0d510a51912e077
PtaZeta <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-02-26 11:43:32 +0100

    factorias y seeders

--- aeropuerto/database/factories/VueloFactory.php
+++ aeropuerto/database/factories/VueloFactory.php
@@ -17,7 +17,7 @@ class VueloFactory extends Factory
         $fecha_salida = fake()->dateTimeBetween('+1 days', '+1 month');
         return [
             'codigo' => strtoupper(fake()->randomLetter()) . strtoupper(fake()->randomLetter()) . fake()->randomNumber(4, true),
-            'origen' => strtoupper(fake()->lexify('???')), // Genera un código IATA de 3 letras aleatorias
+            'origen' => strtoupper(fake()->lexify('???')),
             'destino' => strtoupper(fake()->lexify('???')),
             'airline' => fake()->company(),
             'fecha_salida' => $fecha_salida,
